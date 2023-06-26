dd8824a640bd93e1d5d7035c2b32ce27d7d812eb
iqbalsalik <sisalik84@gmail.com>
2023-06-26 12:59:36 +0530

    forgot password frontEnd

--- Public/login.js
+++ Public/login.js
@@ -1,9 +1,16 @@
 const logIn = document.getElementById("login");
 const email = document.getElementById("logInEmail");
 const password = document.getElementById("logInPassword");
+const forgotPasswordButton = document.getElementById("forgotPasswordButton");
+const forgotPasswordForm  = document.getElementById("forgotPassword");
+const forgotPasswordEmail = document.getElementById("forgotPasswordEmail");
+const forgotPasswordSubmit = document.getElementById("forgotPasswordSubmit");
+const loginForm = document.getElementById("logInForm");
+const signInPage = document.getElementById("signUpHelp");
+const forgotPasswordHelp = document.getElementById("forgotPasswordHelp");
 
 logIn.addEventListener("click",async function(e){
-    e.preventDefault();
+    e.preventDefault()
     try{
         const userDetails = {
             emailId:email.value,
@@ -15,4 +22,26 @@ logIn.addEventListener("click",async function(e){
     } catch (err){
         document.write(err.response.data)
     }  
+})
+
+forgotPasswordButton.addEventListener("click", function(e){
+    e.preventDefault();
+    loginForm.style.display = "none";
+    signInPage.style.display = "none";
+    forgotPasswordHelp.style.display = "none";
+    forgotPasswordForm.style.display = "block";
+})
+
+forgotPasswordSubmit.addEventListener("click",async function(e){
+    try{
+        e.preventDefault();
+        const token = localStorage.getItem("token");
+        if(!forgotPasswordEmail.value){
+            return document.write(`<h1>Email Field is Mandatory!!`)
+        }
+        const result = await axios.post("http://localhost:3000/password/forgotpassword",{email:forgotPasswordEmail.value},{headers:{"authorization":token}})
+    } catch(err){
+        console.log(err)
+    }
+   
 })
